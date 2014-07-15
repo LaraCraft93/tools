b706fc8776ccf3d0a8a692fd99f1483ee2d9f7d2
Lara Maia <lara@craft.net.br>
2014-07-15 06:22:13 -0300

    Add formatting: img, tool, txt

--- copy-genmon.sh
+++ copy-genmon.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # Lara Maia (C) 2014 - <lara@craft.net.br>
+# v2
 
-command_ret=$(echo $1 | sudo -S cat /root/.copy/status.txt)
+command_ret=$(echo "$1" | sudo -S cat /root/.copy/status.txt)
+config_ret=$(echo "$1" | sudo -S cat /root/.copy/config.ini)
+icon="$HOME"/.genmon-icon/copy.png
+
+echo "<img>$icon</img>"
+echo "<tool>$config_ret</tool>"
 
 if [[ $command_ret == *Downloading* ]]; then
     command_ret=${command_ret##*/sec (}
-    echo "Sincronizando (falta ${command_ret%% remaining*})"
+    echo "<txt>Sincronizando (falta ${command_ret%% remaining*})</txt>"
 else
-    echo Sincronizado
+    echo "<txt>Sincronizado</txt>"
 fi
