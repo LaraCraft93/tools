3c8bc7d7793688b5bf83b18f5034366da9982c66
Lara Maia <lara@craft.net.br>
2014-07-15 23:49:30 -0300

    [copy-genmon.sh] Add support for comparing status

--- copy-genmon.sh
+++ copy-genmon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Lara Maia (C) 2014 - <lara@craft.net.br>
-# v2
+# v2.1
 
 command_ret=$(echo "$1" | sudo -S cat /root/.copy/status.txt)
 config_ret=$(echo "$1" | sudo -S cat /root/.copy/config.ini)
@@ -12,6 +12,9 @@ echo "<tool>$config_ret</tool>"
 if [[ $command_ret == *Downloading* ]]; then
     command_ret=${command_ret##*/sec (}
     echo "<txt>Sincronizando (falta ${command_ret%% remaining*})</txt>"
+elif [[ $command_ret == *Comparing* ]]; then
+    command_ret=${command_ret##*items (}
+    echo "<txt>Comparando (falta ${command_ret%% remaining*})</txt>"
 else
     echo "<txt>Sincronizado</txt>"
 fi
