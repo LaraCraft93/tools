948222659ffe691a5a3c10f1c72bc637cc6bb005
Lara Maia <lara@craft.net.br>
2015-05-08 04:12:05 -0300

    Update linux_stub_loader.sh

--- linux_stub_loader.sh
+++ linux_stub_loader.sh
@@ -5,18 +5,17 @@
 EFIROOT=/dev/sda
 EFIPART=1
 
-LABEL="Arch Linux Ck"
-KERNEL=/vmlinuz-linux-ck
-INITRD=/initramfs-linux-ck.img
-SYSTEMROOT=/dev/sda2
-EXTRAOPTS="rw resume=/dev/sda2 resume_offset=15296512 threadirqs"
+LABEL="Lara Gentoo"
+KERNEL=/kernel
+SYSTEMROOT=/dev/sda3
+EXTRAOPTS="rw video=uvesafb:1920x1080-32,mtrr:3,ywrap"
 INIT="/usr/lib/systemd/systemd"
 
 #################################
 
 test $(id -u) == 0 || exit 1
 efibootmgr -b 0000 -B >/dev/null
-efibootmgr -d $EFIROOT -p $EFIPART -c -L "$LABEL" -l $KERNEL -u "root=$SYSTEMROOT initrd=$INITRD init=$INIT $EXTRAOPTS"
+efibootmgr -d $EFIROOT -p $EFIPART -c -L "$LABEL" -l $KERNEL -u "root=$SYSTEMROOT init=$INIT $EXTRAOPTS" >/dev/null
 efibootmgr -O >/dev/null
 efibootmgr -o 0000,0001 >/dev/null
 efibootmgr -n 0000 >/dev/null
