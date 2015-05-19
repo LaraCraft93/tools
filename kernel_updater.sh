7f27632d51e14a8a31deed3a27b8294f06820750
Lara Maia <lara@craft.net.br>
2015-05-19 03:25:34 -0300

    [kernel_updater.sh] Some Fixes

--- kernel_updater.sh
+++ kernel_updater.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # Lara Maia <lara@craft.net.br> 2015
 
+set -e
+
 if test $(id -u) != 0
 then
     echo "Você precisa executar como ROOT!"
     exit 1
 fi
-
-function checkexec() {
-    eval "$@" || exit 1
-}; trap 'checkexec' DEBUG
 
 function checkmv() {
     test -f "$1" && mv "$1" "$2"
@@ -20,19 +18,17 @@ pushd /usr/src/linux
 # limpar source antes da compilação
 make distclean
 
-# aplicar patchs adicionais
-curl -o gcc.patch 'https://raw.githubusercontent.com/graysky2/kernel_gcc_patch/master/enable_additional_cpu_optimizations_for_gcc_v4.9%2B_kernel_v3.15%2B.patch'
-patch -Np1 -i gcc.patch
-
 # Atualizar configuração preferida
 cp /boot/kernel.config /usr/src/linux/.config
 make silentoldconfig
 checkmv /boot/kernel.config /boot/kernel.config.old
-cp .config /boot/kernel.config
 
 # Abrir menu de configuração
 make menuconfig
 #make gconfig
+
+# Fazer backup da configuração
+cp .config /boot/kernel.config
 
 # Compilar o kernel
 make -j5
