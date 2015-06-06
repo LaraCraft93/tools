41a7eae67f2f2be93df71c0e31031e38756996b5
Lara Maia <lara@craft.net.br>
2015-06-06 16:44:26 -0300

    [uniform_root.sh] Update

--- uniform_root.sh
+++ uniform_root.sh
@@ -6,19 +6,24 @@ test $(id -u) -ne 0 && exit 1
 _USER=lara
 _VERBOSE=1
 
+chattr -i /home/lara/.config/geany/geany.conf || exit 1
+
 function link() {
     test $_VERBOSE -eq 1 && v=-v
-    [[ "$1" == *"/"* ]] && (mkdir -p $v ${1%/*} || exit)
-    rm -rf $v "/root/$1"
+    [[ "$1" == *"/"* ]] && (mkdir -p $v ${1%/*} || exit 1)
+    rm -rf $v "/root/$1" || exit 1
     ln -s $v "/home/$_USER/$1" "/root/$1" || exit 1
-    chown $v root:users "/root/$1" || exit 1
-    chgrp -R $v users "/root/$1" || exit 1
-    chmod $v 3777 "/root/$1" || exit 1
-    setfacl -d -m group:users:rwx "/root/$1" || exit 1
-    test $_VERBOSE -eq 1 && getfacl "/root/$1"
+    chown $v -R $_USER:users "/home/$_USER/$1" || exit 1
+    chmod $v 3777 "/home/$_USER/$1" || exit 1
+    setfacl -d -m group:users:rwx "/home/$_USER/$1" || exit 1
+    test $_VERBOSE -eq 1 && getfacl "/home/$_USER/$1"
 }
 
 link .icons
 link .themes
 link .config/xfce4
 link .config/geany
+link .atom
+link .config/Atom
+
+chattr +i /home/lara/.config/geany/geany.conf || exit 1
