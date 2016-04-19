33b9673a9c8bc4d1cf857b72d72d0891c1d86a66
Lara Maia <dev@lara.click>
2016-04-18 22:23:54 -0300

    [linux_stub_loader.sh] Update

--- linux_stub_loader.sh
+++ linux_stub_loader.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Lara Maia <dev@lara.click>
 
 ## CONFIG ########################
 
@@ -9,14 +10,26 @@ LABEL="Lara Gentoo"
 KERNEL=/kernel
 SYSTEMROOT="/dev/sda5"
 EXTRAOPTS="rw"
-INIT="/usr/lib/systemd/systemd"
 
 #################################
 
-test $(id -u) == 0 || exit 1
-efibootmgr -b 0000 -B >/dev/null
-efibootmgr -d $EFIROOT -p $EFIPART -c -L "$LABEL" -l $KERNEL -u "root=$SYSTEMROOT init=$INIT $EXTRAOPTS" >/dev/null
-efibootmgr -O >/dev/null
-efibootmgr -o 0000,0001 >/dev/null
+if test $(id -u) -ne 0; then
+    echo "You must be root!"
+    exit 1
+fi
+
+echo "Deleting old entries..."
+efibootmgr -b 0000 -B 2>&1 >/dev/null
+efibootmgr -b 0001 -B 2>&1 >/dev/null
+efibootmgr -O 2>&1 >/dev/null
+
+echo "Creating new entries"
+efibootmgr -C -d $EFIROOT -p $EFIPART -L "$LABEL" -l $KERNEL -u "root=$SYSTEMROOT $EXTRAOPTS" >/dev/null
+efibootmgr -C -d $EFIROOT -p $EFIPART -L "$LABEL Recovery" -l ${KERNEL}.old -u "root=$SYSTEMROOT $EXTRAOPTS" >/dev/null
+
+echo "Setting boot order"
+efibootmgr -o 0000,0001,0002 >/dev/null
 efibootmgr -n 0000 >/dev/null
+
+echo -e "Done\n"
 efibootmgr -v
