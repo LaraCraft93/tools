cb8a3aeb367a2950f57d600b4b6a8374b9899c22
Lara Maia <dev@lara.click>
2016-08-30 21:01:19 -0300

    [linux_stub_loader] Add emergency kernel label

--- linux_stub_loader.sh
+++ linux_stub_loader.sh
@@ -21,14 +21,16 @@ fi
 echo "Deleting old entries..."
 efibootmgr -b 0000 -B 2>&1 >/dev/null
 efibootmgr -b 0001 -B 2>&1 >/dev/null
+efibootmgr -b 0002 -B 2>&1 >/dev/null
 efibootmgr -O 2>&1 >/dev/null
 
 echo "Creating new entries"
 efibootmgr -C -d $EFIROOT -p $EFIPART -L "$LABEL" -l $KERNEL -u "root=$SYSTEMROOT $EXTRAOPTS" >/dev/null
 efibootmgr -C -d $EFIROOT -p $EFIPART -L "$LABEL Recovery" -l ${KERNEL}.old -u "root=$SYSTEMROOT $EXTRAOPTS" >/dev/null
+efibootmgr -C -d $EFIROOT -p $EFIPART -L "$LABEL Emergency" -l ${KERNEL}.emergency -u "root=$SYSTEMROOT $EXTRAOPTS" >/dev/null
 
 echo "Setting boot order"
-efibootmgr -o 0000,0001,0002 >/dev/null
+efibootmgr -o 0000,0001,0002,0003 >/dev/null
 efibootmgr -n 0000 >/dev/null
 
 echo -e "Done\n"
