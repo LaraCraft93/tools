3ff898002fad32e382edae0290be5ef71bb5cdb0
Lara Maia <dev@lara.click>
2016-10-18 01:01:20 -0200

    linux_stub_loader: Add rootflags

--- linux_stub_loader.sh
+++ linux_stub_loader.sh
@@ -10,6 +10,7 @@ LABEL="Lara Gentoo"
 KERNEL=/kernel
 SYSTEMROOT="PARTUUID=F717E7D8-2E32-4123-AC66-463731E4AE54"
 EXTRAOPTS="rw"
+ROOTFLAGS="data=writeback"
 
 #################################
 
@@ -25,8 +26,8 @@ efibootmgr -b 0002 -B 2>&1 >/dev/null
 efibootmgr -O 2>&1 >/dev/null
 
 echo "Creating new entries"
-efibootmgr -C -d $EFIROOT -p $EFIPART -L "$LABEL" -l $KERNEL -u "root=$SYSTEMROOT $EXTRAOPTS" >/dev/null
-efibootmgr -C -d $EFIROOT -p $EFIPART -L "$LABEL Recovery" -l ${KERNEL}.old -u "root=$SYSTEMROOT $EXTRAOPTS" >/dev/null
+efibootmgr -C -d $EFIROOT -p $EFIPART -L "$LABEL" -l $KERNEL -u "root=$SYSTEMROOT rootflags=$ROOTFLAGS $EXTRAOPTS" >/dev/null
+efibootmgr -C -d $EFIROOT -p $EFIPART -L "$LABEL Recovery" -l ${KERNEL}.old -u "root=$SYSTEMROOT rootflags=$ROOTFLAGS $EXTRAOPTS" >/dev/null
 efibootmgr -C -d $EFIROOT -p $EFIPART -L "$LABEL Emergency" -l ${KERNEL}.emergency -u "root=$SYSTEMROOT $EXTRAOPTS" >/dev/null
 
 echo "Setting boot order"
